341ce6771ad709b1b8a060fe172bddabefae0720
princepatrickbondzie <ngekow@gmail.com>
2022-11-25 10:54:54 +0000

    number changed

--- src/components/BannerTwo.js
+++ src/components/BannerTwo.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function BannerTwo() {
   return (
       <div className='px-4 lg:h-12 w-[90%] py-4 xl:h-12 mx-auto flex justify-center rounded-md items-center lg:w-[52rem] xl:w-[52rem] bg-red-300 shadow-md'>
-          <h1 className=' lg:text-base xl:text-base text-sm'>Send proof of payment to the WhatsApp number <a href="tel:+233552922712"><b>+233552922712</b></a> for swift confirmation and activation.</h1>
+      <h1 className=' lg:text-base xl:text-base text-sm'>Send proof of payment to the WhatsApp number <a href="tel:+233551286706"><b>+233551286706</b></a> for swift confirmation and activation.</h1>
     </div>
   )
 }
